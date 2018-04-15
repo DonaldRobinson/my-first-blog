07834b94d1e9479d21418b701cde8c295c1d6acb
macone <macone@macones-iMac-417.local>
2018-04-15 07:39:37 -0400

    Added Nav Bar, moved it to base.html, added garfield pic

--- blog/views.py
+++ blog/views.py
@@ -9,7 +9,8 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+    posts =Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_detail.html', {'post': post, 'posts': posts})    
 
     
 def gfield_list(request):
