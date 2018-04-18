ed8642546f1b9eb227ffa565d5563e687e5807d5
macone <macone@macones-iMac-417.local>
2018-04-17 23:25:30 -0400

    Added dickey welch and tabbed gallery

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from PIL import Image, ImageFilter
 
 
 class Post(models.Model):
@@ -8,13 +9,14 @@ class Post(models.Model):
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
+    
     published_date = models.DateTimeField(
             blank=True, null=True)
-
     def publish(self):
         self.published_date = timezone.now()
         self.save()
 
     def __str__(self):
         return self.title
+
 
--- blog/views.py
+++ blog/views.py
@@ -10,9 +10,12 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     posts =Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_detail.html', {'post': post, 'posts': posts})    
+    if (post.pk == 1):
+        return render(request, 'blog/post_garfield.html', {'post': post, 'posts': posts})    
 
+    if (post.pk == 2):
+        return render(request, 'blog/garfield.html', {'post': post, 'posts': posts})    
     
 def gfield_list(request):
-    gfield = Post.objects.filter(title__contains='Garfield') 
+    gfield = Post.objects.filter(title='Garfield') 
     return render(request, 'blog/garfield.html', {'gfield': gfield})   
